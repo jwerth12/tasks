662f7becc5bffb80c572b2e72edb13dd3797bb24
Jennifer Werth <jennwerth@jennifers-mbp-2.myfiosgateway.com>
2022-02-10 18:52:31 -0500

    Task 3 edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,6 +8,46 @@ function App(): JSX.Element {
             <header className="App-header">
                 Jenn Werth :) UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>Travel Bucketlist</h1>
+            <img
+                src="https://i2-prod.mirror.co.uk/incoming/article23661643.ece/ALTERNATES/n615/1_Beaty-limestone-rock.jpg"
+                alt="The view I would like to have right now"
+            />
+            <Container>
+                <Row>
+                    <Col>
+                        Still To Do:
+                        <div
+                            style={{
+                                width: 50,
+                                height: 10,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        <ol>
+                            <li>Go island hopping in Thailand</li>
+                            <li>
+                                Roast a marshmallow over a volcano in Guatemala
+                            </li>
+                            <li>Skate on a frozen lake in Canada</li>
+                            <li>Go skydiving in Hawaii</li>
+                        </ol>
+                    </Col>
+                    <Col>
+                        Completed:
+                        <div
+                            style={{
+                                width: 50,
+                                height: 10,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World!
