bd3d16f541d9c2736c98a26225f68368b67bb3d5
Jennifer Werth <jennwerth@Jennifers-MacBook-Pro-2.local>
2022-02-15 16:32:32 -0500

    Created functions

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return ((temperature - 32) * 5) / 9;
 }
 
 /**
@@ -12,7 +12,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let total = 0;
+    if (first > 0) {
+        total += first;
+    }
+    if (second > 0) {
+        total += second;
+    }
+    if (third > 0) {
+        total += third;
+    }
+    return total;
 }
 
 /**
@@ -20,7 +30,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    const upper = message.toUpperCase();
+    return `${upper}!`;
 }
 
 /**
@@ -28,7 +39,8 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    const len: number = message.length;
+    return message[len - 1] === "?";
 }
 
 /**
@@ -37,5 +49,12 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    const word1: string = word.toUpperCase();
+    if (word1 === "YES") {
+        return true;
+    } else if (word1 === "NO") {
+        return false;
+    } else {
+        return null;
+    }
 }
