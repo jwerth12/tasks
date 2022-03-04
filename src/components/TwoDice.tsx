c918b6a3993463b78d27f3755308d1a42c8951f9
Jennifer Werth <jennwerth@jennifers-mbp-2.myfiosgateway.com>
2022-03-03 23:40:57 -0500

    Starting TwoDice component

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,29 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [Left, setLeft] = useState<number>(0);
+    const [Right, setRight] = useState<number>(0);
+    return (
+        <div>
+            <div>
+                <Button onClick={() => setLeft(d6)}>Roll Left</Button>
+                <Button onClick={() => setRight(d6)}>Roll Right</Button>
+            </div>
+            <div>
+                Left: <span data-testid="left-die">{Left} </span>
+                Right: <span data-testid="right-die">{Right} </span>
+            </div>
+            <div>
+                {Left === 0 || Right === 0 ? (
+                    <span>Roll Dice!</span>
+                ) : Right === 1 && Left === 1 ? (
+                    <span>Sorry! Lose</span>
+                ) : Right === Left ? (
+                    <span>Yay! Win</span>
+                ) : (
+                    <span>Roll Again!</span>
+                )}
+            </div>
+        </div>
+    );
 }
