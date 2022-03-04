88ed531d69dbc6f7c4a89ca9f8c4edd8a9075d4b
Jennifer Werth <jennwerth@jennifers-mbp-2.myfiosgateway.com>
2022-03-03 23:39:15 -0500

    Creating change type component

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,28 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [question, setQuestion] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function changeType(): void {
+        setQuestion(
+            question === "multiple_choice_question"
+                ? "short_answer_question"
+                : "multiple_choice_question"
+        );
+    }
+    return (
+        <div>
+            <div>
+                <Button onClick={changeType}>Change Type</Button>
+            </div>
+            <div>
+                {question === "multiple_choice_question" ? (
+                    <span>Multiple Choice</span>
+                ) : (
+                    <span>Short Answer</span>
+                )}
+            </div>
+        </div>
+    );
 }
