5a5c92fb1649f51031dbee35e07586af17838275
Jennifer Werth <jennwerth@wifi-roaming-128-4-82-26.wifi.udel.edu>
2022-03-06 16:22:05 -0500

    fixing shove box

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface boxProps {
+    position: number;
+    setPosition: (num: number) => void;
+}
+
 function ShoveBoxButton({
     position,
     setPosition
 }: {
     position: number;
     setPosition: (newPosition: number) => void;
-}) {
+}): JSX.Element {
     return (
         <Button onClick={() => setPosition(4 + position)}>Shove the Box</Button>
     );
 }
 
-function MoveableBox(): JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+function MoveableBox({ position, setPosition }: boxProps): JSX.Element {
+    //const [position, setPosition] = useState<number>(10);
     return (
         <div
             data-testid="moveable-box"
@@ -32,19 +37,22 @@ function MoveableBox(): JSX.Element {
 }
 
 export function ShoveBox(): JSX.Element {
-    const box = MoveableBox();
-
+    //const box = MoveableBox();
+    const [position, setPosition] = useState<number>(10);
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}</span>
             <div>
                 <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
+                    position={position}
+                    setPosition={setPosition}
                 ></ShoveBoxButton>
-                {box}
-            </div> */}
+            </div>
+            <MoveableBox
+                position={position}
+                setPosition={setPosition}
+            ></MoveableBox>
         </div>
     );
 }
