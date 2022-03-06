6fa92076613e2356eb81f437dc7725c12a982d75
Jennifer Werth <jennwerth@wifi-roaming-128-4-82-26.wifi.udel.edu>
2022-03-06 16:19:33 -0500

    added name to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
@@ -18,43 +18,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 Jenn Werth :) UD CISC275 with React Hooks and TypeScript
             </header>
-            <h1>Travel Bucketlist</h1>
-            <img
-                src="https://i2-prod.mirror.co.uk/incoming/article23661643.ece/ALTERNATES/n615/1_Beaty-limestone-rock.jpg"
-                alt="The view I would like to have right now"
-            />
-            <Container>
-                <Row>
-                    <Col>
-                        Still To Do:
-                        <div
-                            style={{
-                                width: 50,
-                                height: 10,
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                        <ol>
-                            <li>Go island hopping in Thailand</li>
-                            <li>
-                                Roast a marshmallow over a volcano in Guatemala
-                            </li>
-                            <li>Skate on a frozen lake in Canada</li>
-                            <li>Go skydiving in Hawaii</li>
-                        </ol>
-                    </Col>
-                    <Col>
-                        Completed:
-                        <div
-                            style={{
-                                width: 50,
-                                height: 10,
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                    </Col>
-                </Row>
-            </Container>
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
@@ -63,11 +26,11 @@ function App(): JSX.Element {
                 automatically reload. Hello World!
             </p>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
-            <ColoredBox></ColoredBox>
+            {<ColoredBox></ColoredBox>}
             <hr></hr>
             <ShoveBox></ShoveBox>
             <hr></hr>
