cd60b02863fa7d2d4fa83c033d0face53778082c
Jennifer Werth <jennwerth@wifi-roaming-128-4-153-33.wifi.udel.edu>
2022-03-18 16:06:04 -0400

    Creating quizzer section

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import quizzes from "../data/quizzes.json";
-import { Quiz } from "../interfaces/quiz";
-import { Question } from "../interfaces/question";
+import { DisplayQuizzes } from "./DisplayQuizzes";
+//import { Question } from "../interfaces/question";
 
 export type QuestionType = "multiple_choice_question" | "short_answer_question";
 
@@ -12,27 +11,11 @@ A question can be multiple choice or short answer (taken from previous Question 
 A question can either be right or wrong (expected answer vs not)
 */
 
-const QUIZZES = quizzes.map(
-    (quiz): Quiz => ({
-        ...quiz
-        /*         title: quiz.title,
-        description: quiz.description,
-        numQuestions: quiz.numQuestions,
-        questions: quiz.questions.map(
-            (question): Question => ({
-                id: question.id,
-                name: question.name,
-                body: question.body,
-                type: question.type,
-                options: question.options,
-                expected: question.expected,
-                points: question.points,
-                published: question.published
-            })
-        ) */
-    })
-);
-
 export function Quizzer(): JSX.Element {
-    return <h3>Quizzer</h3>;
+    return (
+        <div>
+            <h3>Quizzer</h3>
+            <DisplayQuizzes></DisplayQuizzes>
+        </div>
+    );
 }
