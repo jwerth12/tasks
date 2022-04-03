da41d314763b64b24ec24600508c4404ab441054
Jennifer Werth <jennwerth@Jennifers-MacBook-Pro-2.local>
2022-04-02 22:35:43 -0400

    possible helper function

---
+++ src/quizzer/displayQuestions.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import quizzes from "../data/quizzes.json";
+import { isQuestion } from "../functions";
+import { Quiz } from "../interfaces/quiz";
+import { quizQuestion } from "../interfaces/quizquestion";
+
+export function DisplayQuestions(options: string[]): JSX.Element {
+    const [choice, setChoice] = useState<string>("");
+    const choices: string[] = [];
+    function updateChoice(event: React.ChangeEvent<HTMLInputElement>) {
+        setChoice(event.target.value);
+        // update choices array with this value, index must match the question order
+        //choices[order] = event.target.value;
+    }
+    return (
+        <div>
+            <Form.Check
+                inline
+                key={options[0]}
+                value={options[0]}
+                type="radio"
+                name={options[0]}
+                onChange={updateChoice}
+                id="option-check"
+                label={options[0]}
+                //checked={choice === options[0]}
+            ></Form.Check>
+            <Form.Check
+                inline
+                key={options[1]}
+                value={options[1]}
+                type="radio"
+                name={options[1]}
+                onChange={updateChoice}
+                id="option-check"
+                label={options[1]}
+                //checked={choice === options[1]}
+            ></Form.Check>
+        </div>
+    );
+}
+
+/*
+{options.map((option: string) => (
+                <Form.Check
+                    inline
+                    key={option}
+                    value={option}
+                    type="radio"
+                    name={option}
+                    onChange={updateChoice}
+                    id="option-check"
+                    label={option}
+                    checked={choice === option} // this aint it chief
+                />
+            ))}
+            */
