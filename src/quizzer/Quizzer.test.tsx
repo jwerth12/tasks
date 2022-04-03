580b1fa6be68bd2d2bda35f4724ea17b56093c6e
Jennifer Werth <jennwerth@Jennifers-MacBook-Pro-2.local>
2022-04-02 22:34:44 -0400

    creating test cases

--- src/quizzer/Quizzer.test.tsx
+++ src/quizzer/Quizzer.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Quizzer } from "./Quizzer";
+import { TakeQuiz } from "./TakeQuiz";
 
 describe("Quizzer Tests", () => {
     beforeEach(() => {
@@ -9,5 +10,16 @@ describe("Quizzer Tests", () => {
     test("The Quizzer renders", () => {
         // Up to you to decide what your tests are!
         // Add more tests, more components, more test files!
+        render(<Quizzer />);
+        const quizElement = screen.getByRole("button", { name: /Take Quiz!/i });
+        expect(quizElement).toBeInTheDocument();
     });
 });
+
+/*
+test("renders the course name somewhere", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/CISC275/i);
+    expect(linkElement).toBeInTheDocument();
+});
+*/
