08e4c6390ca568c7e572dec50c45b37985c7a67e
Jennifer Werth <jennwerth@Jennifers-MacBook-Pro-2.local>
2022-04-02 22:39:39 -0400

    fixing linter errors

--- src/quizzer/Quizzer.test.tsx
+++ src/quizzer/Quizzer.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Quizzer } from "./Quizzer";
-import { TakeQuiz } from "./TakeQuiz";
+//import { TakeQuiz } from "./TakeQuiz";
+import userEvent from "@testing-library/user-event";
 
 describe("Quizzer Tests", () => {
     beforeEach(() => {
@@ -13,6 +14,13 @@ describe("Quizzer Tests", () => {
         render(<Quizzer />);
         const quizElement = screen.getByRole("button", { name: /Take Quiz!/i });
         expect(quizElement).toBeInTheDocument();
+    });
+    test("Clicking Take Quiz button displays quiz", () => {
+        const consoleSpy = jest.spyOn(console, "log");
+        render(<Quizzer />);
+        const button = screen.getByRole("button", { name: /Take Quiz!/i });
+        userEvent.click(button);
+        expect(consoleSpy).toHaveBeenCalledWith("Take Quiz");
     });
 });
 
