9178c387a98ae6eed25306cd9fa2ab668da2dc64
Jennifer Werth <jennwerth@jennifers-mbp-2.myfiosgateway.com>
2022-04-03 23:39:23 -0400

    adding test cases

--- src/quizzer/Quizzer.test.tsx
+++ src/quizzer/Quizzer.test.tsx
@@ -3,6 +3,8 @@ import { render, screen } from "@testing-library/react";
 import { Quizzer } from "./Quizzer";
 //import { TakeQuiz } from "./TakeQuiz";
 import userEvent from "@testing-library/user-event";
+import { DisplayQuizzes } from "./DisplayQuizzes";
+import { TakeQuiz } from "./TakeQuiz";
 
 describe("Quizzer Tests", () => {
     beforeEach(() => {
@@ -12,22 +14,46 @@ describe("Quizzer Tests", () => {
         // Up to you to decide what your tests are!
         // Add more tests, more components, more test files!
         render(<Quizzer />);
-        const quizElement = screen.getByRole("button", { name: /Take Quiz!/i });
-        expect(quizElement).toBeInTheDocument();
+        const quizzerHead = screen.getAllByRole("heading", {
+            name: /Quizzer/i
+        });
+        expect(quizzerHead.length).toBeGreaterThanOrEqual(1);
     });
-    test("Clicking Take Quiz button displays quiz", () => {
-        const consoleSpy = jest.spyOn(console, "log");
-        render(<Quizzer />);
-        const button = screen.getByRole("button", { name: /Take Quiz!/i });
-        userEvent.click(button);
-        expect(consoleSpy).toHaveBeenCalledWith("Take Quiz");
-    });
-});
 
-/*
-test("renders the course name somewhere", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/CISC275/i);
-    expect(linkElement).toBeInTheDocument();
+    test("Multiple quizzes are visible", () => {
+        render(<Quizzer />);
+        const quizElement = screen.getAllByRole("button", {
+            name: /Take Quiz!/i
+        });
+        expect(quizElement.length).toBeGreaterThanOrEqual(2);
+    });
+
+    test("Quiz descriptions, numQuestions, etc are visible", () => {
+        render(<Quizzer />);
+        const descriptions = screen.getAllByTestId("quizDetails");
+        const quizElement = screen.getAllByRole("button", {
+            name: /Take Quiz!/i
+        });
+        expect(descriptions.length).toEqual(quizElement.length); // should have same number of descriptions as quizzes displayed
+    });
+
+    test("Quizzes have questions: clicking on quiz shows questions", () => {
+        render(<Quizzer />);
+        const button = screen.getAllByRole("button", { name: /Take Quiz!/i });
+        userEvent.click(button[0]);
+        const quizQuestions = screen.getAllByTestId("question");
+        expect(quizQuestions.length).toBeGreaterThanOrEqual(1);
+        userEvent.click(button[1]);
+        expect(quizQuestions.length).toBeGreaterThanOrEqual(1);
+    });
+
+    test("Clear Answers: can hit clear answers and answers will be reset to blank", () => {
+        render(<Quizzer />);
+        const button = screen.getAllByRole("button", { name: /Take Quiz!/i });
+        userEvent.click(button[0]);
+        const resetbutton = screen.getByRole("button", {
+            name: /Clear Answers/i
+        });
+        userEvent.click(resetbutton);
+    });
 });
-*/
